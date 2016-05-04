9bd86913eec82e25ea5460b89093e04f7df1198e
Rorg Tanev <rorichster@gmail.com>
2016-05-04 23:32:59 +0300

    Add Languages

---
+++ languages/en/messages.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	'add_whatsnew'	=> 'Add New',
+	'description'	=> 'Description',
+	'list_whatsnew'	=> 'List What\'s New',
+	'version'		=> 'Version',
+	'whatsnew'		=> 'What\' New',
+
+];
